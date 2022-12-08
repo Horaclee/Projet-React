23df3de2ea7bef53d44d9d06e04f0c51a1d5e87a
Horaclee <115532821+Horaclee@users.noreply.github.com>
2022-12-08 23:31:42 +0100

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import './index.css';
+
 import App from './App';
 
 import Home from './pages/Home';
@@ -8,6 +10,8 @@ import PopularDesc from './pages/PopularDesc';
 import PopularMovie from './pages/PopularMovie';
 import Toprated from './pages/Toprated';
 import TopratedDescription from './pages/TopRatedDescription';
+import Upcoming from './pages/Upcoming';
+import UpcomingDesc from './pages/UpcomingDesc';
 
 import reportWebVitals from './reportWebVitals';
 import {
@@ -17,27 +21,44 @@ import {
   Link
 } from "react-router-dom";
 
+
+
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home/>,
   },
+
   {
     path:"/popularmovie",
     element: <PopularMovie/>,
   },
+
   {
     path:"/populardesc/:id",
     element: <PopularDesc/>,
   },
+
   {
     path:"/toprated",
     element: <Toprated/>,
   },
+
   {
     path:"/toprateddesc/:id",
     element: <TopratedDescription/>
-  }
+  },
+
+  {
+    path: "/upcoming",
+    element: <Upcoming/>,
+  },
+
+  {
+    path: "/upcomingdesc/:id",
+    element: <UpcomingDesc/>,
+  },
+
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
